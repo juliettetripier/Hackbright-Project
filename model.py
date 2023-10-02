c0d43e6c65036970c2829d8a34db3206d607e9cc
Juliette <juli.tripier@gmail.com>
2023-10-02 19:27:52 -0400

    Initial commit

---
+++ model.py
@@ -0,0 +1,19 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    """A user."""
+
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer,
+                        autoincrement=True,
+                        primary_key=True)
+    email = db.Column(db.String, unique=True)
+    username = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
+
+    
+    
